283949b73e93e4a335c41ac5c5c05c4eaa66a3a5
samvrny <samvrny@gmail.com>
2022-07-16 18:38:12 -0500

    added validation statements

--- app.js
+++ app.js
@@ -14,12 +14,28 @@ return inquirer.prompt([
         {
             type: "input",
             name: "name",
-            message: "What is your name?"
+            message: "What is your name? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log("Please enter your name!");
+                  return false;
+                }
+            }
         },
         {
             type: "input",
-            name: "github",
-            message: "Enter your Github Username"
+            name: "GitHub Username",
+            message: "Enter your Github Username",
+            validate: githubInput => {
+                if(githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub username!");
+                    return false;
+                }
+            }
         },
         {
             type: "input",
@@ -44,13 +60,21 @@ Add a New Project
     return inquirer.prompt([
         {
             type: "input",
-            name: "name",
+            name: "Project name",
             message: "What is the name of your project?"
         },
         {
             type: "input",
             name: "description",
-            message: "Provide a description of the project (Required)"
+            message: "Provide a description of the project (Required)",
+            validate: projectDesc => {
+                if(projectDesc) {
+                    return true;
+                } else {
+                    console.log("Please enter a description of your project!");
+                    return false;
+                }
+            }
         },
         {
             type: "checkbox",
@@ -61,7 +85,15 @@ Add a New Project
         {
             type: "input",
             name: "link",
-            message: "Enter the Github link to your project. (Required)"
+            message: "Enter the Github link to your project. (Required)",
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the link to your github project!");
+                    return false;
+                }
+            }
         },
         {
             type: "confirm",
