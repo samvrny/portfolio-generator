a99c078427f33aed2cc1d14238c88e67dee670a1
samvrny <samvrny@gmail.com>
2022-07-16 13:48:32 -0500

    generated an index file :D

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-console.log(profileDataArgs);
+fs.writeFile("index.html", generatePage(name, github), err => {
+    if(err)throw new Error(err);
 
-const printProfileData = profileDataArr => {
-    // This...
-    for(let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-    }
+    console.log("Portfolio complete! Checkout out index.html to see the output!");
+});
+
+//FOR REFERENCE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// console.log(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for(let i = 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i]);
+//     }
 
 
-console.log("====================");
+// console.log("====================");
 
-//Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// //Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+//FOR REFERENCE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
